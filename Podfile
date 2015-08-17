0133a5bc79e72c7a3b063ff35a06fe54fd1f9f3a
Tim Clem <tim.clem@gmail.com>
2015-08-17 14:19:30 -0400

    Switch to pods for tests. Update for Swift 2

---
+++ Podfile
@@ -0,0 +1,4 @@
+use_frameworks!
+
+pod 'Nimble', :git => 'https://github.com/Quick/Nimble.git', :branch => 'swift-2.0'
+pod 'Quick', :git => 'https://github.com/Quick/Quick.git', :branch => 'swift-2.0'
--- swift-deli/AppDelegate.swift
+++ swift-deli/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var resultDeli = deli
         resultDeli.append(name)
 
-        println("You are number \(deli.count+1)")
+        print("You are number \(deli.count+1)")
 
         return resultDeli
     }
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     {
         var resultDeli = deli
         let servingName = resultDeli.removeAtIndex(0)
-        println("Currently Serving \(servingName)")
+        print("Currently Serving \(servingName)")
         return resultDeli
     }
 
@@ -45,9 +45,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         var result = "The line is currently: "
-        for var index = 0; index<deli.count; ++index
+        for (index, person) in deli.enumerate()
         {
-            result += "\(index+1). \(deli[index])"
+            result += "\(index+1). \(person)"
             if index != deli.count-1
             {
                 result += " "
@@ -55,29 +55,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return result
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
