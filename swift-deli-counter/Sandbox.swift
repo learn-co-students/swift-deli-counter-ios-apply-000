d1db487a8f35f2d517f23f01e507c1e434464820
markedwardmurray <markedwardmurray@gmail.com>
2015-11-02 14:09:33 -0500

    revise solution and tests

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -2,40 +2,36 @@
 
 import Foundation
 
-func takeANumber(deli: [String], name: String) -> [String]
-{
-    var resultDeli = deli
-    resultDeli.append(name)
-    
-    print("You are number \(deli.count+1)")
-    
-    return resultDeli
-}
 
-func nowServing(deli: [String]) -> [String]
-{
-    var resultDeli = deli
-    let servingName = resultDeli.removeAtIndex(0)
-    print("Currently Serving \(servingName)")
-    return resultDeli
-}
-
-func printLine(deli: [String]) -> String
-{
-    if deli.count == 0
-    {
-        return "The line is empty"
+func stringForDeliLine(deliLine: [String]) -> String {
+    
+    if deliLine.count == 0 {
+        return "The line is currently empty."
     }
     
-    var result = "The line is currently: "
-    for (index, person) in deli.enumerate()
-    {
-        result += "\(index+1). \(person)"
-        if index != deli.count-1
-        {
-            result += " "
-        }
+    var result = "The line is:"
+    for var i = 0; i < deliLine.count; i++ {
+        let numberInLine = i + 1
+        result += "\n\(numberInLine). \(deliLine[i])"
     }
+    print(result)
     return result
+}
+    
+func addNameTo(name: String, var deliLine: [String]) -> [String] {
+    deliLine.append(name)
+    print("Welcome \(name)! You are number \(deliLine.count) in line.")
+    return deliLine
+}
+        
+func serveNextCustomerInDeliLine(var deliLine: [String]) -> [String] {
+    
+    if deliLine.count > 0 {
+        let nextCustomer = deliLine.removeFirst()
+        print("Now serving \(nextCustomer)")
+    } else {
+        print("The line is currently empty.")
+    }
+    return deliLine
 }
 
--- swift-deli-counterTests/SandboxSpec.swift
+++ swift-deli-counterTests/SandboxSpec.swift
@@ -6,22 +6,77 @@ import Nimble
 
 class SandboxSpec: QuickSpec {
     override func spec() {
-        let deliLine = ["Ada","Joe"]
-        describe("takeANumber") {
-            it("Adds a person") {
-                expect(takeANumber(deliLine, name: "Chris")).to(equal(["Ada","Joe","Chris"]))
+        var empty: [String] = []
+        var precollege: [String] = []
+        var ios: [String] = []
+        
+        beforeEach {
+            empty = []
+            precollege = ["Victoria", "Danny", "Lyel"]
+            ios = ["Joe", "Tim", "Jim", "Tom"]
+        }
+        
+        describe("stringForDeliLine(_:)") {
+            it("should return a string saying the line is empty when the argument array is empty") {
+                
+                expect(stringForDeliLine(empty)).to(match("The line is currently empty."))
+            }
+            
+            it("should return a string describing the line with three names") {
+                let expected = "The line is:\n1. Victoria\n2. Danny\n3. Lyel"
+                
+                expect(stringForDeliLine(precollege)).to(match(expected))
+            }
+            
+            it("should return a string describing the line with four names") {
+                let expected = "The line is:\n1. Joe\n2. Tim\n3. Jim\n4. Tom"
+                
+                expect(stringForDeliLine(ios)).to(match(expected))
             }
         }
         
-        describe("nowServing") {
-            it("Removes the person") {
-                expect(nowServing(deliLine).count).to(equal(1))
+        describe("addNameTo(_:deliLine:)") {
+            it("adds the name 'Al' to an empty array") {
+                let result = addNameTo("Al", deliLine: empty)
+                let expected = ["Al"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("adds the name 'Dan' to the end of the 'precollege' array") {
+                let result = addNameTo("Dan", deliLine: precollege)
+                let expected = ["Victoria", "Danny", "Lyel", "Dan"]
+                
+                expect(result).to(equal(expected))
+            }
+            
+            it("adds the name 'Mark' to the end of the 'ios' array") {
+                let result = addNameTo("Mark", deliLine: ios)
+                let expected = ["Joe", "Tim", "Jim", "Tom", "Mark"]
+                
+                expect(result).to(equal(expected))
             }
         }
         
-        describe("printLine") {
-            it("Prints correctly") {
-                expect(printLine(deliLine)).to(equal("The line is currently: 1. Ada 2. Joe"))
+        describe("serveNextCustomerInDeliLine(_:)") {
+            it("should return an empty array when an empty array is submitted") {
+                let result = serveNextCustomerInDeliLine(empty)
+                
+                expect(result).to(beEmpty())
+            }
+            
+            it("should remove 'Victoria' from the 'precollege' array") {
+                let result = serveNextCustomerInDeliLine(precollege)
+                let expected = ["Danny", "Lyel"]
+                    
+                expect(result).to(equal(expected))
+            }
+            
+            it("should remove 'Joe' from the 'ios' array") {
+                let result = serveNextCustomerInDeliLine(ios)
+                let expected = ["Tim", "Jim", "Tom"]
+                
+                expect(result).to(equal(expected))
             }
         }
     }
