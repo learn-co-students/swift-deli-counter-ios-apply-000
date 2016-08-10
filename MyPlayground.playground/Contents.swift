1426fd591a5b55d42fa798087a0fa6c54e9a2253
Henry Ly <henryly@gmail.com>
2016-08-10 03:20:28 -0700

    Done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,8 @@
-//  Sandbox.swift
+//: Playground - noun: a place where people can play
 
-import Foundation
+import UIKit
 
-/**
-
- * Write your functions here!
-
- */
+var str = "Hello, playground"
 
 func stringForDeliLine(line: [String]) -> String {
     var deliLineStatus: String = "The line is:"
@@ -20,20 +16,22 @@ func stringForDeliLine(line: [String]) -> String {
     return deliLineStatus
 }
 
-func addName(name: String, toDeliLine: [String]) -> [String] {
-    var newDeliLine = toDeliLine
-    newDeliLine.append(name)
-    print("Hey, \(name)! You are #\(newDeliLine.count) in line!")
-    return newDeliLine
-}
+var currentLine = ["Al", "Chris", "Zach"]
+
+var printMe = stringForDeliLine(currentLine)
+
+print(printMe)
 
 func serveNextCustomerInDeliLine(currentLine: [String]) -> [String] {
     var deliLineMinusOne = currentLine
     if !deliLineMinusOne.isEmpty {
         let nextCustomer = deliLineMinusOne.removeFirst()
         print("Hey, \(nextCustomer)! Welcome to the Deli! What can we get started for you?")
-    } else {
-        print("The line is currently empty.")
     }
     return deliLineMinusOne
 }
+
+let expected = ["Joe", "Tim", "Jim", "Tom", "Mark"]
+
+
+serveNextCustomerInDeliLine(expected)
