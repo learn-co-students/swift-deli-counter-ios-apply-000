c16e6c80540c64d2319e7f58af5c0d5db9274931
Robert Deans <robdeans@gmail.com>
2016-08-12 01:47:23 -0400

    deli counter ready for business

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -8,3 +8,44 @@ import Foundation
 
  */
 
+//#1-3:
+
+var namesInLine = ["Al", "Chris", "Zach"]
+
+func stringForDeliLine(_ deliLine:[String]) -> String {
+    var readout = String()
+    if !deliLine.isEmpty {
+        readout = "The line is:\n"
+        for(index, name) in deliLine.enumerate() {
+            readout = readout + "\(index + 1). \(name)\n"
+        }
+    }
+    else {
+        print("The line is currently empty")
+    }
+    return readout
+}
+
+
+//#4
+
+func addName(_ newCustomer: String, var deliLine: [String]) -> [String] {
+    deliLine.append(newCustomer)
+    print("Welcome \(newCustomer)! Your place in line is \(deliLine.count)")
+   return deliLine
+}
+
+//#5
+
+func serveNExtCustomerInDeliLine(var _ deliLine: [String]) -> [String]{
+    if !deliLine.isEmpty {
+        deliLine.removeFirst()
+    print("Will \(deliLine[0]) please step forward!")
+    return deliLine
+    } else {
+    return deliLine
+    }
+}
+
+//error message that deliLine was a 'let' constant; fixed be declaring a variable in the argument
+
