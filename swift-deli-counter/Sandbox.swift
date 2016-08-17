501b8a71d9e85b0ecb3e4bea2859d7406f6dd5cc
Kyle Hazelton <gabrielhendifar@Kyles-iMac-3rd-Floor-3.local>
2016-08-17 17:30:20 -0400

    Deli Counter's Done

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -2,9 +2,39 @@
 
 import Foundation
 
-/**
 
- * Write your functions here!
 
- */
+ func stringForDeliLine(deliLine:Array<String>) ->Array<String> {
+ var currentLine: [String] = ["The line is:"]
+ var nameEntry = ""
+ if deliLine.isEmpty == true{
+ nameEntry = "The line is currently empty."
+ currentLine[0] = nameEntry
+ } else {
+ for (index,_) in deliLine.enumerate() {
+ nameEntry="\(index+1). \(deliLine[index])"
+ currentLine.append(nameEntry)
+ }
+ }
+ return currentLine
+ }
+
+ func addName(name:String,toDeliLine:Array<String>) ->Array<String> {
+ var newDeliLine = toDeliLine
+ newDeliLine.append(name)
+ print("You are \(newDeliLine.count)th in line")
+ return toDeliLine
+ }
+ 
+ func serveNextCustomerInDeliLine(currentLine:Array<String>) -> Array<String> {
+ var newLine = currentLine
+ if currentLine.isEmpty == false {
+ newLine.removeFirst()
+ print("next customer")
+ } else {
+ print("line's empty")
+ }
+ return newLine
+ }
+ 
 
