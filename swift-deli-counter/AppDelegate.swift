bf6bfc378f020d3be2b73de75cea28753a333ba2
EnriqueTor <etorrendell@gmail.com>
2016-08-25 00:06:22 -0400

    Complete problems

--- swift-deli-counter/AppDelegate.swift
+++ swift-deli-counter/AppDelegate.swift
@@ -11,7 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        // let iosAncestors = ["Al", "Chris", "Zach"]
+//        let iosAncestors = ["Al", "Chris", "Zach"]
         
         // Do not alter
         return true  //
