7ad6ca13704a7f8ae9759d279880ddd820d2ca6c
Alexander Mason <apmason119@gmail.com>
2016-08-29 21:26:07 -0400

    Complete problems

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -7,4 +7,60 @@ import Foundation
  * Write your functions here!
 
  */
+/**
+ 
+ * Takes array. Returns "The line is currently empty." if array is empty. Returns "The line is:" with array values 
+ on subsequent lines
+ 
+ */
+func stringForDeliLine(stringArray: [String]) -> String {
+    var finalLine = ""
+    if stringArray.isEmpty {
+        return("The line is currently empty.")
+    } else {
+        var lineString = ""
+        for (index, name) in stringArray.enumerate() {
+            let nameValue = ("\(index + 1). \(name)")
+            lineString += nameValue + "\n"
+        }
+        finalLine = lineString
+    }
+    return("The line is:" + "\n" + finalLine)
+}
+
+/**
+  
+ * Add's name to array, sends them welcome message, and returns new appended array
+ 
+ */
+
+func addName(name: String,toDeliLine: [String]) -> [String] {
+    var newLine = toDeliLine
+    newLine.append(name)
+    print("Welcome, \(name)! Your place in line is \(newLine.count)" )
+    return newLine
+}
+/**
+ 
+ * Moves next customer up in line by removing 0 index in array. Handles error message if array is empty
+ 
+ */
+
+func serveNextCustomerInDeliLine(deliLine: [String]) -> [String] {
+    var newLine = deliLine
+    if newLine.isEmpty {
+        print("Line is empty")
+    } else {
+        newLine.removeFirst()
+        print("\(newLine[0]), it is your turn")
+    }
+    return newLine
+}
+
+
+
+
+
+
+
 
