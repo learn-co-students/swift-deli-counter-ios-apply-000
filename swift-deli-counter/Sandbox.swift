67c2659a460ddd4d6770a8c1fcdcf0448dd6df18
jaugeni <jaugeni@gmail.com>
2016-09-23 21:18:14 -0400

    Complete problems

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -8,3 +8,36 @@ import Foundation
 
  */
 
+
+func stringForDeliLine(forDeliLine: [String]) -> String {
+    
+    if forDeliLine.isEmpty {
+        return "The line is currently empty."
+    } else {
+        var numerationList = "The line is:"
+        for (index, step) in forDeliLine.enumerate() {
+            numerationList += ("\n\(index + 1). \(step)")
+        }
+        return numerationList
+    }
+    
+}
+
+
+func addName(name: (String),toDeliLine deliLine: [String]) -> [String] {
+    var newDeliLine = deliLine
+    newDeliLine.append(name)
+    print("Wlcome \(name)! You are a \(newDeliLine.count).")
+    return newDeliLine
+}
+
+func serveNextCustomerInDeliLine(guestList: [String]) -> [String] {
+    var newGuestList = guestList
+    
+    if guestList.count > 0 {
+        let customer = newGuestList.removeFirst()
+        print("Next custome \(customer)")
+    }
+    
+    return newGuestList
+}
