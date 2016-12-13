7f74c255dfc60dceaf3885645950647db9b7047d
Kaypree Hodges <kaypree@spencer-macbook.local>
2016-12-13 05:47:52 -0500

    Completed the lab

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -2,9 +2,30 @@
 
 import Foundation
 
-/**
+func stringForDeliLine(_ line: [String]) -> String {
+    var lineString = ""
+    if line.isEmpty {
+        return "The line is currently empty."
+    } else {
+        for (index, name) in line.enumerated() {
+            lineString += "\(index + 1). \(name)\n"
+        }
+        return "The line is:\n\(lineString)"
+    }
+}
 
- * Write your functions here!
+func addName(_ name: String, toDeliLine deliLine: [String]) -> [String] {
+    var newDeliLine = deliLine
+    newDeliLine.append(name)
+    print("Welcome \(name), you are customer number \(newDeliLine.count) today.")
+    return newDeliLine
+}
 
- */
-
+func serveNextCustomerInDeliLine(_ deliLine: [String]) -> [String] {
+    var newDeliLine = deliLine
+    if !deliLine.isEmpty {
+        print("Order up for \(deliLine[0])!")
+        newDeliLine.removeFirst()
+    }
+    return newDeliLine
+}
