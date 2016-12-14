ad4e532adaa5ae529df7ed09aaf444570b161a4d
Carlos <chernandez411@gmail.com>
2016-12-14 11:26:41 -0600

    Submit solution

--- swift-deli-counter/Sandbox.swift
+++ swift-deli-counter/Sandbox.swift
@@ -2,9 +2,32 @@
 
 import Foundation
 
-/**
+func stringForDeliLine(_ names: [String]) -> String {
+    var result = String()
+    if names.isEmpty {
+        return "The line is currently empty."
+    } else {
+        result.append("The line is:")
+        for (index, element) in names.enumerated() {
+            result.append("\n\(index + 1). \(element)")
+        }
+    }
+    return result
+}
 
- * Write your functions here!
+func addName(_ name: String, toDeliLine line: [String]) -> [String] {
+    var result = line
+    result.append(name)
+    print("Welcome \(name). Your place in line is \(result.count)")
+    return result
+}
 
- */
-
+func serveNextCustomerInDeliLine(_ line: [String]) -> [String] {
+    var result = line
+    guard result.count != 0 else {
+        return []
+    }
+    result.removeFirst()
+    print("\(result.first) it is your turn.")
+    return result
+}
