d99d7cc84c1f6aea09e2fddf1d7d1db2c7c9fc7e
edmund-h <447ght@gmail.com>
2017-01-07 04:23:04 -0500

    mmm, Space Ham

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -16,20 +16,54 @@ class SpaceDeli {
     
     // Question #1
  
-    
+    func description () -> String {
+        
+        if line.isEmpty { return "The line is currently empty." }
+            
+        var output = "The line is:"
+        
+        for (count, name) in line.enumerated() {
+            output += "\n\(count + 1). \(name)"
+        }
+        
+        return output
+        
+    }
     
     
     
     // Question #2
 
-    
+    func add (name: String) -> String {
+        
+        if line.isEmpty {
+            line.append(name)
+            return "Welcome \(name). You're first in line!"
+        }
+        
+        let position = line.count + 1
+        
+        line.append(name)
+        
+        return "Welcome \(name). You're number \(position) in line."
+    }
     
     
     
     
     // Question #3
 
-    
+    func serveNextCustomer () -> String {
+        
+        if line.isEmpty { return "The line is empty." }
+        
+        let name = line [0]
+        
+        line.remove( at: 0 )
+        
+        return "\(name) is ready to be served, please come forward."
+        
+    }
     
     
     
