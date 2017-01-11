759ebefcf5d754fdac4572b882d9b986ae47dd79
edmund-h <447ght@gmail.com>
2017-01-11 10:45:50 -0500

    added documentation and made code more efficient

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -18,15 +18,17 @@ class SpaceDeli {
  
     func description () -> String {
         
-        if line.isEmpty { return "The line is currently empty." }
-            
-        var output = "The line is:"
+        if line.isEmpty { return "The line is currently empty." }   //just returns this text if empty
+                                                                    //fallthrough conditional
         
-        for (count, name) in line.enumerated() {
-            output += "\n\(count + 1). \(name)"
+        var output = "The line is:" // string variable for output. not needed if empty
+        
+        for (count, name) in line.enumerated() { //enumerated for loop appends each item in the line
+            output += "\n\(count + 1). \(name)" //along with the numbered place in line to output string
+                                                //count +1 so first place in line isn't 0
         }
         
-        return output
+        return output  //returns the finished string
         
     }
     
@@ -36,17 +38,14 @@ class SpaceDeli {
 
     func add (name: String) -> String {
         
-        if line.isEmpty {
-            line.append(name)
+        line.append(name) //first, adds name input to the line array local variable
+        
+        if line.count == 1 {
             return "Welcome \(name). You're first in line!"
-        }
+        }   //return this text if there is now only one element in the array, otherwise fall through
         
-        let position = line.count + 1
-        
-        line.append(name)
-        
-        return "Welcome \(name). You're number \(position) in line."
-    }
+        return "Welcome \(name). You're number \(line.count) in line."
+    }   //returns new person's name and their place in line.
     
     
     
@@ -55,16 +54,15 @@ class SpaceDeli {
 
     func serveNextCustomer () -> String {
         
-        if line.isEmpty { return "The line is empty." }
+        if line.isEmpty { return "The line is empty." } //return this if no elements in line array
         
-        let name = line [0]
+        let name = line [0]  //falls through and copies first line element into return string const...
         
-        line.remove( at: 0 )
+        line.remove( at: 0 ) //then removes that first line element. for lack of a pop-out method ^___^
         
-        return "\(name) is ready to be served, please come forward."
+        return "\(name) is ready to be served, please come forward." //order up!
         
     }
     
-    
-    
+    //Additional ideas for functionality... Remove walkouts from the middle of the line? An iterating loop alerting each waiting person what their new position in line is when the line changes? Also, what if Space Meg Ryan or Space Billy Crystal walk into the Space Deli? We need to account for this important contigency...
 }
