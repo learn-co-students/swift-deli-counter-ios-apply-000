8aa8249441667cb2b071b2825a8d10bb3ae415c3
sirbimon <bimonaretga@outlook.com>
2017-01-17 12:20:37 -0500

    succesfully ran local tests.

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -15,20 +15,56 @@ class SpaceDeli {
     
     
     // Question #1
- 
+    func description() -> String{
+        
+        var lineStr: String = ""
+        
+        if line.isEmpty != true {
+            lineStr = "The line is:"
+            
+            for (index, person) in line.enumerated() {
+                let linePos = index + 1
+                lineStr = lineStr + "\n\(linePos). \(person)"
+            }
+            
+        } else {
+            lineStr = "The line is currently empty."
+        }
+        
+        return lineStr
+    }
     
     
     
     
     // Question #2
-
+    func add(name: String) -> String {
+        
+        if line.isEmpty {
+            line.append(name)
+            return "Welcome \(name). You're first in line!"
+            
+        } else {
+            line.append(name)
+            return "Welcome \(name). You're number \(line.count) in line."
+            
+        }
+    }
     
     
     
     
     
     // Question #3
-
+    func serveNextCustomer() -> String {
+        if line.isEmpty {
+            return "The line is empty."
+        } else {
+            let serving = line[0]
+            line.remove(at: 0)
+            return "\(serving) is ready to be served, please come forward."
+        }
+    }
     
     
     
