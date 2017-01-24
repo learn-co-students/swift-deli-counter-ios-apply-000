aadd41ac243e6ef5402e48a6ec1923a2345b550e
Oliver-Moffat <oliver.locke.moffat@gmail.com>
2017-01-24 11:52:19 -0500

    Edited Space Deli

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -13,21 +13,52 @@ class SpaceDeli {
     var line: [String] = []
     
     
+    func description() -> String {
     
-    // Question #1
- 
+        if line.isEmpty {
+            return "The line is currently empty."
+        }
+       
+        var order = "The line is:"
+        for (index, place) in line.enumerated() {
+            order.append("\n\(index + 1). \(place)")
+        }
+        return order
+        
+    }
+
     
-    
-    
-    
-    // Question #2
+   
 
     
     
+    func add(name: String) -> String {
+ 
+        line.append(name)
+        
+        if name == line[0] {
+            return "Welcome \(name). You're first in line!"
+        } else {
+            return "Welcome \(name). You're number \(line.count) in line."
+        }
+        
+        
+    }
+    
+
+    
+    func serveNextCustomer() -> String {
+        
+        if line.isEmpty {
+            return "The line is empty."
+        } else {
+            let name = line.remove(at: 0)
+            return ("\(name) is ready to be served, please come forward.")
+        }
+        
+    }
     
     
-    
-    // Question #3
 
     
     
