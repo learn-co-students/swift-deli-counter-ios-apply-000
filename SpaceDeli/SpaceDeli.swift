0a1cd81e5943561e0d02fb32728f9fcec2a7051f
sandboats <akhil.alt7@gmail.com>
2017-01-27 17:04:20 +0530

    swift-deli-counter-ios-apply-000 done

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -16,19 +16,76 @@ class SpaceDeli {
     
     // Question #1
  
-    
+    func description() -> String{
+        
+        print("The line is:")
+        
+        var i = 0
+        
+        if line.count != 0 {
+            
+            for name in line{
+                
+                print("\(i). \(name)")
+                i = i+1
+            }
+            
+        }
+        
+        else {
+            
+            
+           return "The line is currently empty."
+            
+            
+        }
+        
+        
+        return ""
+    }
     
     
     
     // Question #2
 
+    func add(name: String) -> String{
+        
+        if line.count == 0 {
+            line.append(name)
+            return "Welcome \(name). You're first in line!"
+        }
+        
+        line.append(name)
+        
+        var counter = line.count
+
+        
+        return "Welcome \(name). You're number \(counter) in line."
+        
+    }
     
-    
-    
-    
+
     
     // Question #3
 
+    func serveNextCustomer() -> String{
+        
+        if line.count == 0 {
+            
+            return "The line is empty."
+        }
+        
+        else{
+            
+            return "\(line[0]) is ready to be served, please come forward."
+        }
+        
+        
+        
+        
+    }
+    
+    
     
     
     
