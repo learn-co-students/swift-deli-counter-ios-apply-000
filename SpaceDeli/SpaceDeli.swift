0ef7aaf73368955113dc09160f71b814be7fd12f
wjuseck <will.juseck@gmail.com>
2017-02-12 19:40:21 -0500

    Very tough-- had to doublecheck work with other commits

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -16,12 +16,40 @@ class SpaceDeli {
     
     // Question #1
  
+    func description() -> String {
+        
+        
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+            
+            var lineOutput = "The line is:"
+            
+            for (index, lineResident) in line.enumerated() {
+                
+                lineOutput = lineOutput + "\n\(index + 1). \(lineResident)"
+            
+            }
+            return lineOutput
+        }
+    }
+
     
-    
-    
+
     
     // Question #2
 
+    func add(name: String) -> String {
+        if line.isEmpty {
+        line.append(name)
+            return "Welcome \(name). You're first in line!"
+
+        } else {
+            line.append(name)
+            return "Welcome \(name). You're number \(line.count) in line."
+        }
+    }
+    
     
     
     
@@ -30,7 +58,16 @@ class SpaceDeli {
     // Question #3
 
     
-    
-    
-    
+    func serveNextCustomer() -> String {
+        if line.isEmpty {
+            return "The line is empty."
+        } else {
+            let str = line.remove(at: 0)
+            
+            line.remove(at: 0)
+            
+            return "\(str) is ready to be served, please come forward."
+            
+        }
+    }
 }
