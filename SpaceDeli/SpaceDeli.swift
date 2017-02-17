49ef8418f8f371a54e7bd14e1092c8d0820f3cbf
Dramalho7 <Dramalho7@gmail.com>
2017-02-17 10:35:47 -0500

    That was really really hard

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -16,21 +16,41 @@ class SpaceDeli {
     
     // Question #1
  
-    
-    
-    
+    func description () -> String {
+        if line.isEmpty {
+            
+            return "The line is currently empty."
+            
+        } else {
+            
+            return "The line is:\n1. \(line[0])\n2. \(line[1])\n3. \(line[2])"
+        }
+        
+    }
     
     // Question #2
 
-    
+    func add(name: String) -> String {
+       line.append(name)
+        
+        var message = "Welcome \(name). "
+        message += line.count == 1 ? "You're first in line!" : "You're number \(line.count) in line."
+        return message
+    }
     
     
     
     
     // Question #3
 
-    
+    func serveNextCustomer() -> String {
+        guard !line.isEmpty else { return "The line is empty." }
+        let next = line.removeFirst()
+        return "\(next) is ready to be served, please come forward."
+    }
+
     
     
     
 }
+
