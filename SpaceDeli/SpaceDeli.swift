62db0f3702fca3d912fdcdbd4294cd51dceee9fb
Patrick C <patrickco17@gmail.com>
2017-03-14 12:22:34 -0500

    Completed lab

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -12,22 +12,56 @@ class SpaceDeli {
     
     var line: [String] = []
     
+     func description() -> String {
+        
+        var names: String = ""
+        let ammountOfPeople = line.count - 1
+        
+        if line.isEmpty {
+            names = "The line is currently empty."
+            return names
+        }
+        names = "The line is:\n"
+        for (index,people) in line.enumerated() {
+            if index == ammountOfPeople {
+                names = names + "\(index + 1). \(people)"
+            } else {
+                names = names + "\(index + 1). \(people)\n"
+            }
+        }
+    return names
+}
     
     
-    // Question #1
- 
-    
-    
-    
-    
-    // Question #2
+    func add(name: String) -> String {
+        var sentence = ""
+        
+        if line.isEmpty {
+            line.append(name)
+            sentence = "Welcome \(name). You're first in line!"
+        } else {
+            line.append(name)
+            sentence = "Welcome \(name). You're number \(line.count) in line."
+        }
+        return sentence
+}
+
+   
+    func serveNextCustomer() -> String {
+        var sentence = ""
+        var name: String
+        
+        if line.isEmpty {
+            sentence = "The line is empty."
+            return sentence
+        }
+        name = line.remove(at: 0)
+        sentence = "\(name) is ready to be served, please come forward."
+        return sentence
+    }
 
     
-    
-    
-    
-    
-    // Question #3
+
 
     
     
