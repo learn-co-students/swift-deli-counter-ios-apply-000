03d5807533635f401fe86b2214b1d849ae005207
Bhuvana Raghu <bhuv.raghu@gmail.com>
2017-03-15 15:47:05 -0400

    SpaceDeli.xcodeproj/project.xcworkspace/contents.xcworkspace

--- SpaceDeli/SpaceDeli.swift
+++ SpaceDeli/SpaceDeli.swift
@@ -11,26 +11,63 @@ import Foundation
 class SpaceDeli {
     
     var line: [String] = []
-    
+    var name: String = ""
     
     
     // Question #1
- 
-    
-    
+    func description() -> String
+    {
+        
+        if line.isEmpty {
+            return "The line is currently empty."
+        }
+        else {
+            var retval = "The line is:"
+            for (index, people) in line.enumerated() {
+                retval = retval + "\n\(index + 1). \(people)"
+            }
+            return retval
+        }
+    }
     
     
     // Question #2
-
     
-    
-    
+    func add(name: String) -> String {
+        
+        line.append(name)
+        
+        if name == line[0] {
+            return "Welcome \(name). You're first in line!"
+        }
+            
+        else {
+            
+            var retvaltwo = "Welcome \(name)."
+            
+            let nextIndex = (line.count);
+            retvaltwo = retvaltwo + " You're number \(nextIndex) in line."
+            return retvaltwo
+            
+        }
+        
+    }
     
     
     // Question #3
-
     
-    
-    
-    
+    func serveNextCustomer() -> String {
+        
+        let Count = line.count
+        if Count == 0 {
+            return "The line is empty." }
+            
+        else {
+            
+            let nextCustomer: String = line[0]
+            let result = "\(nextCustomer) is ready to be served, please come forward."
+            line.remove(at: 0)
+            return result
+        }
+    }
 }
